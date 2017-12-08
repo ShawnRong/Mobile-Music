fbbaeb55b87c9ca903129aa3b473f6f3b504c3d0
ShawnRong <rongshawn@gmail.com>
2017-12-08 23:20:19 +0800

    Add slider

--- scripts/components/slider.js
+++ scripts/components/slider.js
@@ -10,10 +10,10 @@ class Slider {
 
   render(){
     this.el.innerHTML = `<div class="ui-slider-group"></div>`
+    let group = this.el.firstElementChild
     //获取group
-    this.group =  this.el.firstElementChild
-    this.group.style.width = `${this.slides.length * 100}%`
-    this.group.innerHTML = this.slides.map(slide =>
+    group.style.width = `${this.slides.length * 100}%`
+    group.innerHTML = this.slides.map(slide =>
       `
         <div class="ui-slider-item">
           <a href="${slide.link}">
@@ -23,17 +23,15 @@ class Slider {
       `
     ).join('')
 
-    // this.el.innerHTML += `<div></div>`
-    // this.el.innerHTML +=
-    // `
-    //   <p class="ui-slider-dots">
-    //     <b class="ui-state-active"></b>
-    //     <b class=""></b>
-    //     <b class=""></b>
-    //     <b class=""></b>
-    //     <b class=""></b>
-    //   </p>
-    // `
+    let dotsHTML =
+    `
+      <p class="ui-slider-dots">
+        ${this.slides.map((dot, index)=>{
+          return index === 0 ? `<b class="ui-state-active"></b>` : `<b class=""></b>`
+        }).join('')}
+      </p>
+    `
+    this.el.innerHTML += dotsHTML
   }
 
   play(){
@@ -41,15 +39,29 @@ class Slider {
   }
 
   next(){
-    this.index += 1;
+    //FIXME: 最后一张平顺的过度到第一张
+    let group =  this.el.firstElementChild
+    this.index += 1
     //最后一张回原位
     if(this.index === this.slides.length){
-      this.group.style.transform = `translate(0)`
+      group.style.transform = `translate(0)`
       this.index = 0
+      this.dotClass()
       return
     }
     let position = `-${this.index / this.slides.length * 100}%`
-    this.group.style.transform = `translate(${position})`
+    group.style.transform = `translate(${position})`
+
+    this.dotClass()
+  }
+
+  dotClass(){
+    let dots = this.el.lastElementChild
+
+    let children = this.el.lastElementChild.children
+    Array.prototype.map.call(children, child=>child.classList.remove(`ui-state-active`))
+
+    this.el.lastElementChild.children[this.index].className = `ui-state-active`
   }
 }
 
